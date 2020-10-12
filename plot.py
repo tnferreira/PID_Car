6b4398dfd63f3cca8f41d3287dc366e3c6bb28d5
ssdias <stiven.dias@embraer.com.br>
2020-10-12 17:50:25 -0300

    Improving plots.

--- plot.py
+++ plot.py
@@ -30,14 +30,6 @@ class Plot:
         self.speed_set_point_deque = np.zeros(number_samples)
         self.steering_set_point_deque = np.zeros(number_samples)
 
-        self.speed_proportional_term_deque = np.zeros(number_samples)
-        self.speed_derivative_term_deque = np.zeros(number_samples)
-        self.speed_integral_term_deque = np.zeros(number_samples)
-
-        self.steering_proportional_term_deque = np.zeros(number_samples)
-        self.steering_derivative_term_deque = np.zeros(number_samples)
-        self.steering_integral_term_deque = np.zeros(number_samples)
-
         self.ang_accel_x_deque = np.zeros(number_samples)
         self.ang_accel_y_deque = np.zeros(number_samples)
         self.ang_accel_z_deque = np.zeros(number_samples)
@@ -54,6 +46,14 @@ class Plot:
         self.lin_vel_y_deque = np.zeros(number_samples)
         self.lin_vel_z_deque = np.zeros(number_samples)
 
+        self.speed_proportional_term_deque = np.zeros(number_samples)
+        self.speed_derivative_term_deque = np.zeros(number_samples)
+        self.speed_integral_term_deque = np.zeros(number_samples)
+
+        self.steering_proportional_term_deque = np.zeros(number_samples)
+        self.steering_derivative_term_deque = np.zeros(number_samples)
+        self.steering_integral_term_deque = np.zeros(number_samples)
+
         # Initialization
         self.create_figures()
         self.setup_figures()
@@ -66,21 +66,13 @@ class Plot:
         lw = 1  # Line width
 
         # ----- Figure 1 ----- #
-        self.fig1, self.fig1_axs = plt.subplots(6, 1)
+        self.fig1, self.fig1_axs = plt.subplots(4, 1)
         self.lines.append(self.fig1_axs[0].plot([], lw=lw, label='Speed')[0])
         self.lines.append(self.fig1_axs[1].plot([], lw=lw, label='Gear')[0])
         self.lines.append(self.fig1_axs[2].plot([], lw=lw, label='Throttle')[0])
         self.lines.append(self.fig1_axs[3].plot([], lw=lw, label='Steering')[0])
         self.lines.append(self.fig1_axs[0].plot([], lw=lw, label='Speed Set Point')[0])
         self.lines.append(self.fig1_axs[3].plot([], lw=lw, label='Steering Set Point')[0])
-
-        self.lines.append(self.fig1_axs[4].plot([], lw=lw, label='Speed Proportional Term')[0])
-        self.lines.append(self.fig1_axs[4].plot([], lw=lw, label='Speed Derivative Term')[0])
-        self.lines.append(self.fig1_axs[4].plot([], lw=lw, label='Speed Integral Term')[0])
-
-        self.lines.append(self.fig1_axs[5].plot([], lw=lw, label='Steering Proportional Term')[0])
-        self.lines.append(self.fig1_axs[5].plot([], lw=lw, label='Steering Derivative Term')[0])
-        self.lines.append(self.fig1_axs[5].plot([], lw=lw, label='Steering Integral Term')[0])
 
         # ----- Figure 2 ----- #
         self.fig2, self.fig2_axs = plt.subplots(4, 1)
@@ -100,7 +92,17 @@ class Plot:
         self.lines.append(self.fig2_axs[3].plot([], lw=lw, label='Lin_Vel_y')[0])
         self.lines.append(self.fig2_axs[3].plot([], lw=lw, label='Lin_Vel_z')[0])
 
-        self.figs_axs = [self.fig1_axs, self.fig2_axs]
+        # ----- Figure 3 ----- #
+        self.fig3, self.fig3_axs = plt.subplots(6, 1)
+        self.lines.append(self.fig3_axs[0].plot([], lw=lw, label='Speed Proportional Term')[0])
+        self.lines.append(self.fig3_axs[1].plot([], lw=lw, label='Speed Derivative Term')[0])
+        self.lines.append(self.fig3_axs[2].plot([], lw=lw, label='Speed Integral Term')[0])
+
+        self.lines.append(self.fig3_axs[3].plot([], lw=lw, label='Steering Proportional Term')[0])
+        self.lines.append(self.fig3_axs[4].plot([], lw=lw, label='Steering Derivative Term')[0])
+        self.lines.append(self.fig3_axs[5].plot([], lw=lw, label='Steering Integral Term')[0])
+
+        self.figs_axs = [self.fig1_axs, self.fig2_axs, self.fig3_axs]
 
     def setup_figures(self):
         # Adjust axis limits
@@ -109,11 +111,13 @@ class Plot:
         # Adjust plot layout
         self.fig1.tight_layout()
         self.fig2.tight_layout()
+        self.fig3.tight_layout()
 
         # Draw Canvas.
         # Note: the first draw comes before setting data
         self.fig1.canvas.draw()
         self.fig2.canvas.draw()
+        self.fig3.canvas.draw()
 
         # Cache the background
         if self.blit:
@@ -124,6 +128,10 @@ class Plot:
             self.fig2_axs_background = []
             for ax in self.fig2_axs:
                 self.fig2_axs_background.append(self.fig2.canvas.copy_from_bbox(ax.bbox))
+
+            self.fig3_axs_background = []
+            for ax in self.fig3_axs:
+                self.fig3_axs_background.append(self.fig3.canvas.copy_from_bbox(ax.bbox))
 
     def adjust_axis_limits(self):
         for fig_axs in self.figs_axs:
@@ -146,14 +154,6 @@ class Plot:
         self.fig1_axs[3].set_ylim([-0.5, 0.5])
         # ax2.legend()
 
-        self.fig1_axs[4].set_ylabel('Speed PID Terms')
-        #self.fig1_axs[4].set_ylim([-0.5, 0.5])
-        # ax2.legend()
-
-        self.fig1_axs[5].set_ylabel('Steering PID Terms')
-        # self.fig1_axs[5].set_ylim([-0.5, 0.5])
-        # ax2.legend()
-
         self.fig2_axs[0].set_ylabel('Ang. Accel.')
         # fig2_ax1.set_ylim([-2.5e8, 2.5e8])
         # ax3.legend()
@@ -169,6 +169,30 @@ class Plot:
         self.fig2_axs[3].set_ylabel('Lin. Vel.')
         self.fig2_axs[3].set_ylim([-100, 100])
         # ax3.legend()
+
+        self.fig3_axs[0].set_ylabel('Speed P Term')
+        # self.fig3_axs[0].set_ylim([speed_proportional_term_deque.min(), speed_proportional_term_deque.max()])
+        # ax2.legend()
+
+        self.fig3_axs[1].set_ylabel('Speed I Term')
+        # self.fig3_axs[1].set_ylim([speed_integral_term_deque.min(), speed_integral_term_deque.max()])
+        # ax2.legend()
+
+        self.fig3_axs[2].set_ylabel('Speed D Term')
+        # self.fig3_axs[2].set_ylim([speed_derivative_term_deque.min(), speed_derivative_term_deque.max()])
+        # ax2.legend()
+
+        self.fig3_axs[3].set_ylabel('Steering P Term')
+        # self.fig3_axs[3].set_ylim([steering_proportional_term_deque.min(), steering_proportional_term_deque.max()])
+        # ax2.legend()
+
+        self.fig3_axs[4].set_ylabel('Steering I Term')
+        # self.fig3_axs[4].set_ylim([steering_integral_term_deque.min(), steering_integral_term_deque.max()])
+        # ax2.legend()
+
+        self.fig3_axs[5].set_ylabel('Steering D Term')
+        # self.fig3_axs[5].set_ylim([steering_derivative_term_deque.min(), steering_derivative_term_deque.max()])
+        # ax2.legend()
 
     def update(self, car):
         car_state = car.state
@@ -283,6 +307,12 @@ class Plot:
         # fig2_ax2.set_ylim([y[5].min(), y[5].max()])
         # fig2_ax3.set_ylim([y[8].min(), y[8].max()])
         # fig2_ax4.set_ylim([y[11].min(), y[11].max()])
+        self.fig3_axs[0].set_ylim([self.y[6].min(), self.y[6].max()])
+        self.fig3_axs[1].set_ylim([self.y[7].min(), self.y[7].max()])
+        self.fig3_axs[2].set_ylim([self.y[8].min(), self.y[8].max()])
+        self.fig3_axs[3].set_ylim([self.y[9].min(), self.y[9].max()])
+        self.fig3_axs[4].set_ylim([self.y[10].min(), self.y[10].max()])
+        self.fig3_axs[5].set_ylim([self.y[11].min(), self.y[11].max()])
 
         if self.blit:
             # In this post http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
@@ -296,6 +326,9 @@ class Plot:
             for ax in self.fig2_axs_background:
                 self.fig2.canvas.restore_region(ax)
 
+            for ax in self.fig3_axs_background:
+                self.fig3.canvas.restore_region(ax)
+
             # Redraw just the points
             self.fig1_axs[0].draw_artist(self.lines[0])
             self.fig1_axs[1].draw_artist(self.lines[1])
@@ -303,14 +336,6 @@ class Plot:
             self.fig1_axs[3].draw_artist(self.lines[3])
             self.fig1_axs[2].draw_artist(self.lines[4])
             self.fig1_axs[3].draw_artist(self.lines[5])
-
-            self.fig1_axs[4].draw_artist(self.lines[6])
-            self.fig1_axs[4].draw_artist(self.lines[7])
-            self.fig1_axs[4].draw_artist(self.lines[8])
-
-            self.fig1_axs[5].draw_artist(self.lines[9])
-            self.fig1_axs[5].draw_artist(self.lines[10])
-            self.fig1_axs[5].draw_artist(self.lines[11])
 
             self.fig2_axs[0].draw_artist(self.lines[12])
             self.fig2_axs[0].draw_artist(self.lines[13])
@@ -328,6 +353,14 @@ class Plot:
             self.fig2_axs[3].draw_artist(self.lines[22])
             self.fig2_axs[3].draw_artist(self.lines[23])
 
+            self.fig3_axs[0].draw_artist(self.lines[6])
+            self.fig3_axs[1].draw_artist(self.lines[7])
+            self.fig3_axs[2].draw_artist(self.lines[8])
+
+            self.fig3_axs[3].draw_artist(self.lines[9])
+            self.fig3_axs[4].draw_artist(self.lines[10])
+            self.fig3_axs[5].draw_artist(self.lines[11])
+
             # Fill in the axes rectangle
             for ax in self.fig1_axs:
                 self.fig1.canvas.blit(ax.bbox)
@@ -335,13 +368,18 @@ class Plot:
             for ax in self.fig2_axs:
                 self.fig2.canvas.blit(ax.bbox)
 
+            for ax in self.fig3_axs:
+                self.fig3.canvas.blit(ax.bbox)
+
         else:
             # Redraw everything
             self.fig1.canvas.draw()
             self.fig2.canvas.draw()
+            self.fig3.canvas.draw()
 
         self.fig1.canvas.flush_events()
         self.fig2.canvas.flush_events()
+        self.fig3.canvas.flush_events()
 
         # print('Mean Frame Rate: {fps:.3f}FPS'.format(fps=((self.i + 1) / (time.time() - self.t_start))))
         # self.i += 1
