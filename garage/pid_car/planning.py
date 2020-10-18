8b8d40bc969bf87776f9e3226cf350787b0f7832
ssdias <stiven.dias@embraer.com.br>
2020-10-18 15:36:15 -0300

    New path planning class.

--- garage/pid_car/planning.py
+++ garage/pid_car/planning.py
@@ -5,6 +5,10 @@
 import os, time, pickle, airsim, math
 from pynput import keyboard # Run $pip install pynput in the Anaconda prompt
 from skoods import utils
+import numpy as np
+from rdp import rdp
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 class Waypoints:
     def __init__(self, car_name):
@@ -91,3 +95,194 @@ class Behavior:
         if dist_from_start_point > 1.0 and self.mode != 'CRUZE':
             self.mode = 'CRUZE'
             print(self.car.name + " || BEHAVIOR: Mode = CRUZE")
+
+
+class PathPlanner:
+    """"
+    This class implements a very straightforward path planning method. It builds a reference position & velocity profile
+    by approximating the recorded trajectory by multiple line segments and provides a mean to select the next waypoint
+    along this profile according to the current vehicle position.
+    """
+    def __init__(self, epsilon=1, sample_time=0.01, number_samples=500, min_distance=3):
+        """
+        Default constructor.
+
+        :param epsilon: maximum deviation in meters of recorded waypoints from the approximated line segments
+        :param sample_time: sampling time in seconds to employed to record the waypoints
+        :param number_samples: number of vehicle state samples to keep (presentation only)
+        :param min_distance: minimum distance to determine if a waypoint along the reference profile was reached
+        """
+        # Parameters
+        self.epsilon = epsilon
+        self.sample_time = sample_time
+        self.number_samples = number_samples
+        self.min_distance = min_distance
+
+        # Recorded waypoints are stored for presentation purposes
+        self.recorded_waypoints_x = []
+        self.recorded_waypoints_y = []
+        self.recorded_waypoints_v = []
+        self.recorded_waypoints_t = []
+
+        # Vehicle states are stored for presentation purposes
+        self.vehicle_position_x = np.zeros(number_samples)
+        self.vehicle_position_y = np.zeros(number_samples)
+        self.vehicle_speed = np.zeros(number_samples)
+        self.vehicle_track_angle = np.zeros(number_samples)
+
+        # Initialize the reference profile
+        self.reference_profile_waypoints_x = []
+        self.reference_profile_waypoints_y = []
+        self.reference_profile_waypoints_v = []
+
+        # Initialize auxiliary indexes
+        self.initial_search_index = 0
+        self.last_waypoint_index = 0
+
+    def update_reference_profile_from_recorded_waypoints(self, recorded_waypoints_x, recorded_waypoints_y,
+                                                         recorded_waypoints_v):
+        """
+        Build the reference profile using the recorded waypoints position and speed.
+
+        :param recorded_waypoints_x: recorded waypoints position X coordinates
+        :param recorded_waypoints_y: recorded waypoints position Y coordinates
+        :param recorded_waypoints_v: recorded waypoints velocity magnitude
+        :return: None
+        """
+        # Get recorded waypoints' positions and speeds
+        self.recorded_waypoints_x = np.array(recorded_waypoints_x)
+        self.recorded_waypoints_y = np.array(recorded_waypoints_y)
+        self.recorded_waypoints_v = np.array(recorded_waypoints_v)
+
+        n = self.recorded_waypoints_v.size
+        tmin = 0
+        tmax = (n - 1) * self.sample_time
+        self.recorded_waypoints_t = np.linspace(tmin, tmax, num=n)
+
+        # Approximate the waypoints by line segments
+        xy = np.vstack((self.recorded_waypoints_x, self.recorded_waypoints_y)).T
+        mask = rdp(xy, self.epsilon, return_mask=True)
+        mask[-1] = False
+
+        # Set the reference profile
+        self.reference_profile_waypoints_x = self.recorded_waypoints_x[mask]
+        self.reference_profile_waypoints_y = self.recorded_waypoints_y[mask]
+        self.reference_profile_waypoints_v = self.recorded_waypoints_v[mask]
+
+        # Reset auxiliary indexes
+        self.initial_search_index = 0
+        self.last_waypoint_index = 0
+
+    def get_next_reference_profile_waypoint(self, current_vehicle_position_x, current_vehicle_position_y,
+                                            current_vehicle_speed, current_vehicle_track_angle):
+        """
+        Select the next waypoint along the reference profile.
+
+        :param current_vehicle_position_x: current vehicle position X coordinate
+        :param current_vehicle_position_y: current vehicle position Y coordinate
+        :param current_vehicle_speed: current vehicle velocity magnitude
+        :param current_vehicle_track_angle: current vehicle velocity orientation
+        :return: Next waypoint position and velocity; a flag indicating if a new lap was completed
+        """
+        length = len(self.reference_profile_waypoints_v)
+        final_search_index = -1  # define as last element
+
+        # Roll buffers
+        self.vehicle_position_x = np.roll(self.vehicle_position_x, -1)
+        self.vehicle_position_y = np.roll(self.vehicle_position_y, -1)
+        self.vehicle_speed = np.roll(self.vehicle_speed, -1)
+        self.vehicle_track_angle = np.roll(self.vehicle_track_angle, -1)
+
+        # Fill buffers
+        self.vehicle_position_x[-1] = current_vehicle_position_x
+        self.vehicle_position_y[-1] = current_vehicle_position_y
+        self.vehicle_speed[-1] = current_vehicle_speed
+        self.vehicle_track_angle[-1] = current_vehicle_track_angle
+
+        # Compute squared distances to all waypoint from the initial search index to the last one
+        dx = [current_vehicle_position_x - wpx for wpx in self.reference_profile_waypoints_x[self.initial_search_index:final_search_index]]
+        dy = [current_vehicle_position_y - wpy for wpy in self.reference_profile_waypoints_y[self.initial_search_index:final_search_index]]
+        d = [ix ** 2 + iy ** 2 for (ix, iy) in zip(dx, dy)]
+
+        # Get the index to the closest one ahead
+        nearest_waypoint_distance = min(d)
+        nearest_waypoint_index = self.initial_search_index + d.index(nearest_waypoint_distance)
+
+        # Check whether the selected waypoint was reached or not
+        if nearest_waypoint_distance < self.min_distance ** 2:
+            if nearest_waypoint_index >= length - 1:
+                nearest_waypoint_index = 0
+            else:
+                nearest_waypoint_index += 1
+
+        # Check end of waypoints index
+        if nearest_waypoint_index >= length - 2:
+            self.initial_search_index = 0
+        else:
+            self.initial_search_index = nearest_waypoint_index + 1
+
+        # Check whether the lap was completed or not
+        completed_lap = self.last_waypoint_index > nearest_waypoint_index
+        self.last_waypoint_index = nearest_waypoint_index
+
+        #  print("nearest_waypoint_index: " + str(nearest_waypoint_index))
+        #  print("last_waypoint_index: " + str(self.last_waypoint_index))
+        #  print("initial_search_index: " + str(self.initial_search_index))
+
+        # Get selected waypoint position and speed
+        next_waypoint_x = self.reference_profile_waypoints_x[nearest_waypoint_index]
+        next_waypoint_y = self.reference_profile_waypoints_y[nearest_waypoint_index]
+        next_waypoint_v = self.reference_profile_waypoints_v[nearest_waypoint_index]
+
+        return next_waypoint_x, next_waypoint_y, next_waypoint_v, completed_lap
+
+    def show_reference_profile(self):
+        """
+        Plot the reference profile along the recorded points; indicates the next waypoint and the vehicle trajectory.
+        :return: None
+        """
+
+        # Get recorded points as line segments
+        points = np.array([self.recorded_waypoints_x, self.recorded_waypoints_y]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        # Create two axis
+        fig, axs = plt.subplots(2, 1)
+
+        # Create a continuous norm to map from data points to colors
+        norm = plt.Normalize(self.recorded_waypoints_v.min(), self.recorded_waypoints_v.max())
+        lc = LineCollection(segments, cmap='jet', norm=norm)
+
+        # Set the values used for colormapping
+        lc.set_array(self.recorded_waypoints_v)
+        lc.set_linewidth(6)
+        line = axs[0].add_collection(lc)
+        fig.colorbar(line, ax=axs[0])
+
+        # Get selected waypoint position and speed
+        last_waypoint_x = self.reference_profile_waypoints_x[self.last_waypoint_index]
+        last_waypoint_y = self.reference_profile_waypoints_y[self.last_waypoint_index]
+        last_waypoint_v = self.reference_profile_waypoints_v[self.last_waypoint_index]
+
+        # Get last vehicle position and speed
+        last_vehicle_position_x = self.vehicle_position_x[-1]
+        last_vehicle_position_y = self.vehicle_position_y[-1]
+        last_vehicle_speed = self.vehicle_speed[-1]
+        last_vehicle_track_angle = self.vehicle_track_angle[-1]
+
+        # Plot recorded position profile
+        axs[0].set_xlim(self.recorded_waypoints_x.min(), self.recorded_waypoints_x.max())
+        axs[0].set_ylim(self.recorded_waypoints_y.min(), self.recorded_waypoints_y.max())
+        axs[0].set_xlabel('x [m]')
+        axs[0].set_ylabel('y [m]')
+        axs[0].plot(self.reference_profile_waypoints_x, self.reference_profile_waypoints_y, 'k--', marker='o')
+        axs[0].plot([last_vehicle_position_x, last_waypoint_x], [last_vehicle_position_y, last_waypoint_y], 'g-', marker='o')
+
+        # Plot recorded speed profile
+        axs[1].plot(self.recorded_waypoints_t, self.recorded_waypoints_v)
+        axs[1].set_xlim(self.recorded_waypoints_t.min(), self.recorded_waypoints_t.max())
+        axs[1].set_ylim(self.recorded_waypoints_v.min(), self.recorded_waypoints_v.max())
+        axs[1].set_xlabel('t [s]')
+        axs[1].set_ylabel('v [m/s]')
+
+        plt.show()
--- test3.py
+++ test3.py
@@ -5,60 +5,22 @@ from matplotlib.collections import LineCollection
 from matplotlib.colors import ListedColormap, BoundaryNorm
 from rdp import rdp
 
+# Load recorded racing waypoints from file
 waypoints_correction = [0, 0]
 waypoints = planning.Waypoints("")
 waypoints.loadWaypointsFromFile("run-fast4.pickle")
 waypoints_x, waypoints_y, waypoints_v = waypoints.waypointsToLists(waypoints_correction)
 
-x = np.array(waypoints_x)
-y = np.array(waypoints_y)
-v = np.array(waypoints_v)
 
-xy = np.vstack((x, y)).T
-reducedXY = rdp(xy, epsilon=1)
-reducedX = reducedXY[:, 0]
-reducedY = reducedXY[:, 1]
+current_vehicle_position_x = waypoints_x[0] + 2
+current_vehicle_position_y = waypoints_y[0]
+current_vehicle_speed = waypoints_v[0]
+current_vehicle_track_angle = 0
 
-n = v.size
-sample_time = 0.01
-tmin = 0
-tmax = (n-1)*sample_time
-t = np.linspace(tmin, tmax, num=n)
+path_planner = planning.PathPlanner(epsilon=1, sample_time=0.01, number_samples=500, min_distance=3)
+path_planner.update_reference_profile_from_recorded_waypoints(waypoints_x, waypoints_y, waypoints_v)
+next_waypoint_x, next_waypoint_y, next_waypoint_y, completed_lap = path_planner.get_next_reference_profile_waypoint(
+    current_vehicle_position_x, current_vehicle_position_y, current_vehicle_speed, current_vehicle_track_angle)
 
+path_planner.show_reference_profile()
 
-#fig, ax = plt.subplots()
-#ax.plot(x, y)
-#plt.show()
-
-# Create a set of line segments so that we can color them individually
-# This creates the points as a N x 1 x 2 array so that we can stack points
-# together easily to get the segments. The segments array for line collection
-# needs to be (numlines) x (points per line) x 2 (for x and y)
-points = np.array([x, y]).T.reshape(-1, 1, 2)
-segments = np.concatenate([points[:-1], points[1:]], axis=1)
-
-fig, axs = plt.subplots(2, 1)
-
-# Create a continuous norm to map from data points to colors
-norm = plt.Normalize(v.min(), v.max())
-lc = LineCollection(segments, cmap='jet', norm=norm)
-# Set the values used for colormapping
-lc.set_array(v)
-lc.set_linewidth(6)
-line = axs[0].add_collection(lc)
-fig.colorbar(line, ax=axs[0])
-axs[0].set_xlim(x.min(), x.max())
-axs[0].set_ylim(y.min(), y.max())
-axs[0].set_xlabel('x [m]')
-axs[0].set_ylabel('y [m]')
-
-axs[0].plot(reducedX,reducedY, 'k--', marker='o')
-
-# Use a boundary norm instead
-axs[1].plot(t,v)
-axs[1].set_xlim(t.min(), t.max())
-axs[1].set_ylim(v.min(), v.max())
-axs[1].set_xlabel('t [s]')
-axs[1].set_ylabel('v [m/s]')
-
-plt.show()
