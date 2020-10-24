df37c450a94984b9d0deb5ec686e042b9e7455d1
ssdias <stiven.dias@embraer.com.br>
2020-10-24 18:16:21 -0300

    New trajectory waypoints.

--- test3.py
+++ test3.py
@@ -8,7 +8,7 @@ from rdp import rdp
 # Load recorded racing waypoints from file
 waypoints_correction = [0, 0]
 waypoints = planning.Waypoints("")
-waypoints.loadWaypointsFromFile("run-fast4.pickle")
+waypoints.loadWaypointsFromFile("run-fast4-new.pickle")
 waypoints_x, waypoints_y, waypoints_v = waypoints.waypointsToLists(waypoints_correction)
 
 
@@ -19,8 +19,8 @@ current_vehicle_track_angle = 0
 
 path_planner = planning.PathPlanner(epsilon=0.25, sample_time=0.01, number_samples=500, min_distance=6)
 path_planner.update_reference_profile_from_recorded_waypoints(waypoints_x, waypoints_y, waypoints_v)
-next_waypoint_x, next_waypoint_y, next_waypoint_v, completed_lap = path_planner.get_next_reference_profile_waypoint(
+next_waypoint_x, next_waypoint_y, next_waypoint_v, curr_segment_d, curr_segment_a, completed_lap = path_planner.get_next_reference_profile_waypoint(
     current_vehicle_position_x, current_vehicle_position_y, current_vehicle_speed, current_vehicle_track_angle)
 
-path_planner.show_reference_profile()
+path_planner.show_reference_profile(show_full_circuit=True)
 
