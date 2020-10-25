fe470879e11a6236d79a3b1a7d455932fd380f7d
Fabio Oliveira <carvalho.fabio.oliveira@gmail.com>
2020-10-24 21:14:31 -0300

    pausa de simulacao no pid check

--- pid_check.py
+++ pid_check.py
@@ -28,7 +28,7 @@ track_angle_step_samples = math.floor(track_angle_step_duration / sample_time)
 pid_car1 = pid_car.Car(client, sample_time, 'SetCarName1', '2', filename='run-fast4.pickle') 
 
 # Create the plotter
-p = Plot(blit=True)
+p = Plot(blit=True, sample_interval=sample_time)
 
 # Get initial track angle
 initial_track_angle = np.deg2rad(-90.0) #pid_car1.getCurrentTrackAngle()
@@ -37,25 +37,38 @@ print("initial_track_angle: " + str(np.rad2deg(initial_track_angle)) + " [deg]")
 # Apply a speed step keeping the current track angle
 for x in range(speed_step_samples):
     pid_car1.drive(speed_step_amplitude, initial_track_angle)
+    client.simPause(False)
+    time.sleep(sample_time)
+    client.simPause(True)
     p.update(pid_car1)
+
     #print("current_track_angle: " + str(np.rad2deg(pid_car1.getCurrentTrackAngle())) + " [deg]")
 
 print(" Apply a track angle step to the right keeping the speed target: " + str(np.rad2deg(track_angle_step_amplitude)))
 # Apply a track angle step to the right keeping the speed target
 for x in range(track_angle_step_samples):
     pid_car1.drive(speed_step_amplitude, initial_track_angle + track_angle_step_amplitude)
+    client.simPause(False)
+    time.sleep(sample_time)
+    client.simPause(True)
     p.update(pid_car1)
 
 # Apply a track angle step to the left keeping the speed target
 print(" Apply a track angle step to the left keeping the speed target: " + str(np.rad2deg(-track_angle_step_amplitude)))
 for x in range(track_angle_step_samples):
     pid_car1.drive(speed_step_amplitude, initial_track_angle - track_angle_step_amplitude)
+    client.simPause(False)
+    time.sleep(sample_time)
+    client.simPause(True)
     p.update(pid_car1)
 
 # # Stop the car
 print(" Stop the car ")
 for x in range(speed_step_samples):
     pid_car1.drive(0, initial_track_angle)
+    client.simPause(False)
+    time.sleep(sample_time)
+    client.simPause(True)
     p.update(pid_car1)
 
 pid_car1.resetControls()
